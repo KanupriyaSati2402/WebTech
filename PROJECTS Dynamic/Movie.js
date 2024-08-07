d7641a010c8bce816e4e9bf35b241347d7712f8b
[Kanupriya <ksati6331@gmail.com>
2024-08-07 21:18:35 +0530

    New

--- PROJECTS Dynamic/Movie.js
+++ PROJECTS Dynamic/Movie.js
@@ -1,7 +1,25 @@
+let a = document.querySelector(".cards");
 let fetchMovie = async()=>{
     let res = await fetch("https://freetestapi.com/api/v1/movies");
     let data = await res.json();
     console.log(data);
-  
+    data.forEach((e)=>{
+        displayCards(e)
+        })
 }
 fetchMovie();
+
+
+let displayCards=(e)=>{
+ let card=document.createElement("div");
+ card.setAttribute("class","card")
+ card.innerHTML = `<h2>${e.title}</h2>
+                   <p>${e.actors}</p>
+                   <p>Rating : ${e.rating}</p>`
+                  
+                   a.append(card);
+    }
+  
+
+
+
