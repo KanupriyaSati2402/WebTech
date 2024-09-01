8f47e1ee843ffe206d4c24037fe4c997cbe8b461
[Kanupriya <ksati6331@gmail.com>
2024-09-01 21:35:03 +0530

    New

--- Routing/src/Components/Nav1.jsx
+++ Routing/src/Components/Nav1.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import photo1 from "../Assets/photo1.jpg"
 
 const Nav1 = () => {
   return (
-    <div>
-      <Link to="/home/about">About page</Link>
-      <Link to="/home/contact">Contact</Link>
-      <Link to="/home/login">Login page</Link>
-      <Link to="/home/register">Register page</Link>
-    </div>
+    <section className="Nav">
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/home/about">About page</Link>
+          </li>
+          <li>
+            <Link to="/home/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/home/login">Login page</Link>
+          </li>
+          <li>
+            <Link to="/home/register">Register page</Link>
+          </li>
+        </ul>
+      </div>
+      <article className="content">
+      <div className="page">
+       <h1>Welcome to Home page !!</h1>
+      </div>
+      <div className="image">
+        <img src={photo1} className="photo1"/>
+      </div>
+      </article>
+    </section>
+    
   );
 };
 
